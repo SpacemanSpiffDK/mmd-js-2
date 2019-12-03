efb910f31ff88f7f59c9b00777c091802b6f6cb0
Dan Høegh <dan.hoegh@gmail.com>
2019-12-03 13:28:48 +0100

    removed window.onload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,50 @@
 // JS by Dan Høegh
 // UCN MMD 2018
 
-window.onload = function() {
 
-	// Comments section
+// Comments section
 
-	var myCheckbox = document.querySelector('#hasComments');
-	var commentSection = document.querySelector('#commentsection');
+const myCheckbox = document.querySelector('#hasComments');
+const commentSection = document.querySelector('#commentsection');
 
-	myCheckbox.addEventListener('change', function(){
-		if (myCheckbox.checked) {
-			commentSection.classList.add('visible');
-		} else {
-			commentSection.classList.remove('visible');
-		}
-	});
-
-	// submit button
-
-	var acceptTerms = document.querySelector('#acceptTerms');
-	var submitBtn = document.querySelector('#submitBtn');
-
-	acceptTerms.addEventListener('change', function(){
-		if (acceptTerms.checked) {
-			submitBtn.disabled = false;
-		} else {
-			submitBtn.disabled = true;
-		}
-	});
-
-
-	// Select value
-
-	var showLink = document.querySelector('#showLink');
-
-	showLink.addEventListener('click', function(event){
-		event.preventDefault();
-		let theValue = document.querySelector('#mySelect').value;
-		document.querySelector('#selectResult').innerHTML = theValue;
-	});
-
-
-	// select do something to all input elements
-
-	var inputs = document.querySelectorAll('input');
-
-	for (var i = 0; i < inputs.length; i++) {
-		inputs[i].classList.add('dark');	
+myCheckbox.addEventListener('change', function(){
+	if (myCheckbox.checked) {
+		commentSection.classList.add('visible');
+	} else {
+		commentSection.classList.remove('visible');
 	}
-	
+});
+
+// submit button
+
+const acceptTerms = document.querySelector('#acceptTerms');
+const submitBtn = document.querySelector('#submitBtn');
+
+acceptTerms.addEventListener('change', function(){
+	if (acceptTerms.checked) {
+		submitBtn.disabled = false;
+	} else {
+		submitBtn.disabled = true;
+	}
+});
+
+
+// Select value
+
+const showLink = document.querySelector('#showLink');
+
+showLink.addEventListener('click', function(event){
+	event.preventDefault();
+	const theValue = document.querySelector('#mySelect').value;
+	document.querySelector('#selectResult').innerHTML = theValue;
+});
+
+
+// select do something to all input elements
+
+const inputs = document.querySelectorAll('input');
+
+for (var i = 0; i < inputs.length; i++) {
+	inputs[i].classList.add('dark');	
 }
+	
